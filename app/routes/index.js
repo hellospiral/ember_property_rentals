f5daf2b25baecc04c2056784d75696d94400dab4
Matt Carlson <hellospiral@hotmail.com>
2016-08-15 13:41:52 -0700

    add component to update rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -15,6 +15,16 @@ export default Ember.Route.extend({
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
+    },
+
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) { // For each key in the params,
+        if(params[key]!==undefined) { // if it is not undefined,
+          rental.set(key, params[key]); // take the rental and set the property that matches the current key to the value of the current key
+        }
+      });
+      rental.save(); // after looping through all of the keys, save the rental
+      this.transitionTo('index'); // transition to the index route
     }
   }
 });
